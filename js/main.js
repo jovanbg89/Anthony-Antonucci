9a7a3bc5595f36af7af0fc4945359be311399549
Jovan Radmilovic <jovanbg89@gmail.com>
2016-09-06 02:45:35 +0200

    Home page pri kraju izrade

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-
+$(window).on('resize', function() {
+    if($(window).height() < 1200) {
+        $('#home').addClass('.hvr-icon-buzz-out');
+        $('#home').removeClass('.hvr-underline-from-center');
+    }else{
+        $('#home').addClass('.hvr-underline-from-center');
+        $('#home').removeClass('.hvr-icon-buzz-out');
+    }
+});
+$(document).ready(function(){
+    $("#home").click(function(){
+        alert("The paragraph was clicked.");
+    });
+});
